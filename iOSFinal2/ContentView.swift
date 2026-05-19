696769c3a69390f525c826606cbc27df7db1d453
jose <josersanchez0117@gmail.com>
2026-05-19 14:14:39 -0500

    Initial Commit

---
+++ iOSFinal2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSFinal2
+//
+//  Created by jose on 5/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSFinal2/iOSFinal2App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSFinal2App.swift
+//  iOSFinal2
+//
+//  Created by jose on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct iOSFinal2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
